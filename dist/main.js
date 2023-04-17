0be1245bbbfe87505f8a513609572f57e1c790e8
Juan Sebastian Urrea Lopez <jurrea49@hotmail.com>
2023-04-16 20:16:20 -0500

    Taller 2

--- dist/main.js
+++ dist/main.js
@@ -4,10 +4,27 @@ series.forEach(serie => {
     let row = document.createElement("tr");
     row.innerHTML = `
         <th scope="row">${serie.id}</th>
-        <td><a href="${serie.url}" class="text-decoration-none">${serie.name}</a></td>
+        <td><a href="#" class="text-decoration-none">${serie.name}</a></td>
         <td>${serie.channel}</td>
         <td>${serie.seasons}</td>
     `;
+    row.onclick = () => {
+        let card = document.getElementById("card-detail");
+        if (card === null)
+            return;
+        card.classList.remove("d-none");
+        card.innerHTML = `
+        <div class="card">
+            <img src="${serie.image}" class="card-img-top" alt="Imagen for ${serie.name}">
+            <h2 class="card-header bg-primary text-white">${serie.name}</h2>
+            <div class="card-body">
+                <p class="card-text">${serie.description}</p>
+                <div class="text-center">
+                    <a class="text-decoration-none" href="${serie.url}">${serie.url}</a>
+                </div>
+            </div>
+        </div>`;
+    };
     (_a = document.getElementById("table-body")) === null || _a === void 0 ? void 0 : _a.appendChild(row);
 });
 let average = document.getElementById("average");
